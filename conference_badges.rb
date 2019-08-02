1457b87d5ce7aa1e3fa006b910fb6c80355e75e5
Marianne Pyon <mariannepyon@gmail.com>
2019-08-02 17:08:11 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  array = [ ]
+    attendees.each do |name|
+      array.push(badge_maker(name))
+    end
+  array
+end
+
+def assign_rooms(attendees)
+  array = [ ]
+  counter = 1
+  attendees.each do |name|
+    array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  array
+end
+
+def printer(attendees)
+array = [ ]
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
+end
